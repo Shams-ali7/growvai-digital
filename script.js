d1d82d5d6804ab04c78b1ea3406aa879adf99e2f
Growvia Digital <shamsriaz512@gmail.com>
2026-05-21 10:30:10 +0500

    fixed js

--- script.js
+++ script.js
@@ -344,3 +344,14 @@ window.addEventListener('load', () => {
   }
 
 });
+fetch('https://jsonplaceholder.typicode.com/users/1')
+  .then(response => response.json())
+  .then(data => {
+
+    console.log(data);
+
+    console.log(data.name);
+
+    console.log(data.email);
+
+  });
